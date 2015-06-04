40c31451861d3bb8958b589be9ac8b0d44fc6b9f
Christine Perinchery <christine.perinchery@hudl.com>
2015-06-04 15:22:30 -0500

    Initial Commit

---
+++ SuperCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperCard
+//
+//  Created by Christine Perinchery on 6/4/15.
+//  Copyright (c) 2015 Stanford Lessons App House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by Christine Perinchery on 6/4/15.
+//  Copyright (c) 2015 Stanford Lessons App House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
